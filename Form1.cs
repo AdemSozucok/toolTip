aba78c10e2e4c47181ff60958f7b625ac28eb3c7
AdemSozucok <100554124+AdemSozucok@users.noreply.github.com>
2022-03-22 09:40:56 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tool_tip_uygulama1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.SetToolTip(txtAd, "Adınızı giriniz.");
+            tt.SetToolTip(txtSoyAd, "Soyadınızı giriniz.");
+            tt.SetToolTip(txtDTarih, "Doğum tarihinizi gg/aa/yyyy şeklinde giriniz.");
+            tt.SetToolTip(txtAdres, "Adresinizi giriniz.");
+            tt.SetToolTip(btnKaydet, "Kaydetmek için tıklayınız.");
+}
+        }
+    }
+
